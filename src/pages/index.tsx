1fff0e8f3c3209e916dff171c983cc28e4a022a6
kemull <431600134@qq.com>
2021-07-26 17:43:10 +0800

    <新功能>:测试
    # 影响范围:
    
    # 我修改的原因:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,15 @@
  * @Author: KeMull
  * @Date: 2021-07-23 12:04:02
  * @LastEditors: KeMull
- * @LastEditTime: 2021-07-23 16:38:39
+ * @LastEditTime: 2021-07-26 17:42:54
  */
 import styles from './index.less';
-import {Button} from  "antd-mobile"
+import { Button } from 'antd-mobile';
 export default function IndexPage() {
   return (
     <div>
       <h1 className={styles.title}>Page index</h1>
-      <Button type='primary'>21321312</Button>
+      <Button type="primary">21321312</Button>
     </div>
   );
 }
