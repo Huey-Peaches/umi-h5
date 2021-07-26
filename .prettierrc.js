9465288b249da1ddc649e7d4911a5e0101d14124
kemull <431600134@qq.com>
2021-07-26 17:45:18 +0800

    <新功能>:合并prettierrc
    # 影响范围:
    
    # 我修改的原因:

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,11 +2,21 @@
  * @Author: KeMull
  * @Date: 2021-07-26 16:51:22
  * @LastEditors: KeMull
- * @LastEditTime: 2021-07-26 17:37:34
+ * @LastEditTime: 2021-07-26 17:44:26
  */
 // @ts-ignore
 const fabric = require('@umijs/fabric');
 module.exports = {
   ...fabric.prettier,
-  rules: {},
+  rules: {
+    singleQuote: true,
+    trailingComma: 'all',
+    printWidth: 80,
+    overrides: [
+      {
+        files: '.prettierrc',
+        options: { parser: 'json' },
+      },
+    ],
+  },
 };
